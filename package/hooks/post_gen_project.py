3a4644f4b151b1b9613e264eb01e330145ccd1a0
Marcin Paliwoda <paliwoda.marcin@zoho.com>
2022-04-20 08:18:01 +0200

    Add `generate_requirements_file.py` to files to be removed when not using docker

--- package/hooks/post_gen_project.py
+++ package/hooks/post_gen_project.py
@@ -1,7 +1,13 @@
 import os
 
 PROJECT_DIRECTORY = os.path.realpath(os.path.curdir)
-DOCKER_FILES = ["Dockerfile", "docker-compose.yml", "run_tests_in_docker.sh", ".dockerignore"]
+DOCKER_FILES = [
+    "Dockerfile",
+    "docker-compose.yml",
+    "run_tests_in_docker.sh",
+    ".dockerignore",
+    "generate_requirements_file.py",
+]
 
 
 def remove(filepath):
@@ -11,7 +17,7 @@ def remove(filepath):
 if __name__ == "__main__":
     to_remove = []
 
-    if '{{cookiecutter.add_test_dockerfile}}' != "y":
+    if "{{cookiecutter.add_test_dockerfile}}" != "y":
         to_remove.extend(DOCKER_FILES)
 
     for filename in to_remove:
