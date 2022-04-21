fa84cd304e300f4bed3208cd1d2570b9cb5051c0
Marcin Paliwoda <paliwoda.marcin@zoho.com>
2022-04-21 19:14:34 +0200

    Parametrize GET test in flask template

--- flask/{{cookiecutter.project_slug}}/tests/api/v1/test_{{cookiecutter.project_slug}}.py
+++ flask/{{cookiecutter.project_slug}}/tests/api/v1/test_{{cookiecutter.project_slug}}.py
@@ -1,5 +1,17 @@
-def test_{{cookiecutter.project_slug}}_returns_default_greeting_when_no_params_provided(test_client):
-    response = test_client.get("/api/v1/{{cookiecutter.project_slug}}")
+import pytest
+
+
+@pytest.mark.parametrize("name,expected_message",[(None, "Hello world"), ("duck", "Hello duck")])
+def test_{{cookiecutter.project_slug}}_returns_correct_response_based_on_params(
+    test_client,
+    name,
+    expected_message,
+):
+    path = "/api/v1/{{cookiecutter.project_slug}}"
+    if name:
+        path += f"?name={name}"
+
+    response = test_client.get(path)
 
     assert response.status_code == 200
-    assert response.json == {"message": "Hello world"}
+    assert response.json == {"message": expected_message}
